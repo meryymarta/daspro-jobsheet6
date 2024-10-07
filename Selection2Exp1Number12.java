ada605d1d8ca1f586e6a65c14d7170e736629e29
meryymarta <meriammartadinata@gmail.com>
2024-10-07 08:24:01 -0700

    push to github

--- Selection2Exp1Number12.java
+++ Selection2Exp1Number12.java
@@ -11,13 +11,17 @@ public class Selection2Exp1Number12 {
         //determine leap year
         if (year % 4 == 0) {
             if (year % 100 != 0) {
-                System.out.println("Lap year");
+                System.out.println("Leap year");
+            } else if (year % 400 == 0) {
+                System.out.println("Leap year");
             } else {
                 System.out.println("Not a leap year");
             }
-        }
-       
+            } else {
+                System.out.println("Not a leap year");
 
-    }} 
+        }
+    } }
+
     
 
