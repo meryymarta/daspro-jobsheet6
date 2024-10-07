899f48a98c14895052b808d0f3c0a699f285a3df
meryymarta <meriammartadinata@gmail.com>
2024-10-07 03:38:29 -0700

    push to github

--- Selection2Exp2Number12.java
+++ Selection2Exp2Number12.java
@@ -20,13 +20,13 @@ public class Selection2Exp2Number12 {
        if (totalAngle ==180){
         if (angle1 == 90 || angle2 == 90 || angle3 ==90) {
             System.out.println(" Right triangle");
-       } else {
-            System.out.println(" Not a right triangle");
+       } else if (angle1 == angle2 && angle2  == angle3) {
+               System.out.println(" Equilateral triangle");
+       } else if (angle1 == angle2|| angle1 == angle3 || angle2 == angle3) {
+               System.out.println(" Isosceles triangle");
        }
-    } else {
-            System.out.println( " Not a triangle");
-
-       }
-
-       }
+        } else {
+               System.out.println(" Not a triangle");
+       }  
     }
+}
